0b02ed3c041a478bfc2ac981a4232ca7ea2fec6b
TaeyeopLee <lee.taeyeop@hotmail.com>
2021-05-25 11:37:47 +0900

    context API

--- src/components/SelectColors.js
+++ src/components/SelectColors.js
@@ -1,33 +1,73 @@
-import React from 'react';
-import { ColorConsumer } from '../contexts/color';
+import React, { Component } from 'react';
+import ColorContext, { ColorConsumer } from '../contexts/color';
 
 
 const colors = ['red','orange','yellow','green','blue','indigo','violet'];
 
-const SelectColors = () => {
-  return (
-    <div>
-      <h2>색상을 선택하세요.</h2>
-      <ColorConsumer>
-        {({ actions }) => (
-          <div style={{display: 'flex' }}>
+class SelectColors extends Component {
+  static contextType = ColorContext;
+
+  handleSetColor = color => {
+    this.context.actions.setColor(color);
+  }
+
+  handleSetSubColor = color => {
+    this.context.actions.setSubcolor(color);
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>색상을 선택하세요.</h2>
+          <div style={{ display: 'flex' }}>
             {colors.map(color => (
               <div
                 key={color}
-                style={{background: color, width: '24px', height: '24px', cursor: 'pointer' }}
-                onClick={() => actions.setColor(color)}
-                onContextMenu={e => {
-                  e.preventDefault(); // 오른쪽 클릭시 메뉴가 뜨는 것을 무시
-                  actions.setSubcolor(color);
+                style={{
+                  background: color,
+                  width:'24px',
+                  height:'24px',
+                  cursor: 'pointer'
                 }}
+                onClick={() => this.handleSetColor(color)}
+                onContextMenu={e => {
+                    e.preventDefault()
+                    this.handleSetSubColor(color)
+                  }
+                }
               />
             ))}
           </div>
-        )}
-      </ColorConsumer>
-      <hr />
-    </div>
-  )
+          <hr />
+      </div>
+    )
+  }
 }
+
+// const SelectColors = () => {
+//   return (
+//     <div>
+//       <h2>색상을 선택하세요.</h2>
+//       <ColorConsumer>
+//         {({ actions }) => (
+//           <div style={{display: 'flex' }}>
+//             {colors.map(color => (
+//               <div
+//                 key={color}
+//                 style={{background: color, width: '24px', height: '24px', cursor: 'pointer' }}
+//                 onClick={() => actions.setColor(color)}
+//                 onContextMenu={e => {
+//                   e.preventDefault(); // 오른쪽 클릭시 메뉴가 뜨는 것을 무시
+//                   actions.setSubcolor(color);
+//                 }}
+//               />
+//             ))}
+//           </div>
+//         )}
+//       </ColorConsumer>
+//       <hr />
+//     </div>
+//   )
+// }
 
 export default SelectColors;
